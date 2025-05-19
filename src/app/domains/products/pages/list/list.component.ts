090f05e8e25f6f55a7c26193c6d5565cb3754875
Erick Altamirano <erickaltamirano07@hotmail.com>
2025-05-19 16:10:11 -0500

    Creación de Headers en Páginas Web con Angular

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { ProductComponent } from './../../components/product/product.component';
 import { Product } from './../../../shared/models/product.model';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-list',
-  imports: [ProductComponent],
+  imports: [ProductComponent, HeaderComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css',
 })
@@ -13,6 +14,27 @@ export class ListComponent {
 
   constructor() {
     const initProduct: Product[] = [
+      {
+        id: Date.now(),
+        title: 'Pro 1',
+        price: 100,
+        img: 'https://picsum.photos/640/640?=23',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 2',
+        price: 100,
+        img: 'https://picsum.photos/640/640?=24',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'Pro 3',
+        price: 100,
+        img: 'https://picsum.photos/640/640?=25',
+        creationAt: new Date().toISOString(),
+      },
       {
         id: Date.now(),
         title: 'Pro 1',
