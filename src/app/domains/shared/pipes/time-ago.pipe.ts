f8a233eb1b175e9e9461f478687e5b8fcdbf07fd
Erick Altamirano <erickaltamirano07@hotmail.com>
2025-05-21 11:02:55 -0500

    Manipulación de Fechas en Angular con Date Functions

--- src/app/domains/shared/pipes/time-ago.pipe.ts
+++ src/app/domains/shared/pipes/time-ago.pipe.ts
@@ -1,12 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { formatDistance } from 'date-fns';
 
 @Pipe({
-  name: 'timeAgo'
+  name: 'timeAgo',
 })
 export class TimeAgoPipe implements PipeTransform {
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string): string {
+    const date = new Date(value);
+    const today = new Date();
+    return formatDistance(today, date);
   }
-
 }
